3ea15caa518b32e599b235424fa7155aaad5dcac
Oluwaseun Ogunsanu <ogunsanu17@gmail.com>
2026-08-02 10:40:58 +0100

    update package import

---
+++ backend/src/tiber/domain/__init__.py
@@ -0,0 +1,10 @@
+from .enums import Channel, NotificationStatus, SendTimeBasis
+from .value_objects import NotificationContent
+
+__all__ = [
+    "Channel",
+    "Notification",
+    "NotificationContent",
+    "NotificationStatus",
+    "SendTimeBasis",
+]
