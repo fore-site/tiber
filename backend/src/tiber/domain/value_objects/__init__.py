5d97a289feb2728203f32c010aa946f76e80f500
Oluwaseun Ogunsanu <ogunsanu17@gmail.com>
2026-09-23 23:21:00 +0100

    update: value objects package import

--- backend/src/tiber/domain/value_objects/__init__.py
+++ backend/src/tiber/domain/value_objects/__init__.py
@@ -1,13 +1,13 @@
 from .blackout_period import BlackoutPeriod
 from .notification_content import NotificationContent
 from .preferences import RecipientPreferences
-from .restricted_window import RestrictedWindow
+from .quiet_hours import QuietHours
 from .topic_title import TopicTitle
 
 __all__ = [
     "BlackoutPeriod",
     "NotificationContent",
+    "QuietHours",
     "RecipientPreferences",
-    "RestrictedWindow",
     "TopicTitle",
 ]
